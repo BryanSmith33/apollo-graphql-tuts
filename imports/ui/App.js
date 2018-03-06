68a5302b531ce668e4c9831c9523acf2ace78644
Bryan Smith <bryan.gsmith33@gmail.com>
2018-03-06 08:43:46 -0700

    left off on video 10

---
+++ imports/ui/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const App = ({ data }) => {
+  if(data.loading) return null
+  return (
+    <div>
+      <h1>{data.hi}!</h1>
+      <ul>
+        {data.resolutions.map(res => <li key={res._id}>{res.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+const hiQuery = gql`
+  {
+    hi
+    resolutions {
+      _id
+      name
+    }
+  }
+`
+
+export default graphql(hiQuery)(App);
